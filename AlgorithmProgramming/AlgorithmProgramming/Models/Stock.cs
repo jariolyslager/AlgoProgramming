14a37063399257e804197542c83a30d8f70a07c4
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2025-03-12 13:57:25 +0100

    Stock model

---
+++ AlgorithmProgramming/AlgorithmProgramming/Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmProgramming.Models
+{
+    public class Stock
+    {
+        public string Ticker { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+
+        public Stock(string ticker, string name, DateTime date, double price)
+        {
+            Ticker = ticker;
+            Name = name;
+            Date = date;
+            Price = price;
+        }
+    }
+}
