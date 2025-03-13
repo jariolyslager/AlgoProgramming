1ecd1393f13f8e4c5ce1e72d374f2667c4130eb8
KevinSpa2 <kevin.spalink@student.nhlstenden.com>
2025-03-13 14:31:34 +0100

    Start jump search algorithm

---
+++ AlgorithmProgramming/AlgorithmProgramming/Search/JumpSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmProgramming.Models;
+
+namespace AlgorithmProgramming.Search
+{
+    public class JumpSearch
+    {
+        public int SearchHistoricValues(int[] arr, int key) 
+        { 
+            int ArraySize = arr.Length;
+            int prev = 0;
+            int step = (int) Math.Sqrt(ArraySize);
+
+            while (step < ArraySize - 1 && arr[step] <= key)
+            {
+                prev = step;
+                step += (int)Math.Sqrt(ArraySize);
+            }
+
+            return 1;
+        }
+    }
+}
