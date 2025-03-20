1270fc965e693ff8749eb6f8be6d906225047d6b
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2025-03-20 11:59:37 +0100

    Use ObservableCollection, add buttons to change between ArrayList and DoublyLinkedList, quicksort button, stopwatch

--- AlgorithmProgramming/AlgorithmProgramming/ViewModels/MainPageViewModel.cs
+++ AlgorithmProgramming/AlgorithmProgramming/ViewModels/MainPageViewModel.cs
@@ -1,16 +1,22 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using AlgorithmProgramming.Models;
+using AlgorithmProgramming.Sorting;
 using CommunityToolkit.Mvvm.Input;
 
 namespace AlgorithmProgramming.ViewModels
 {
     public partial class MainPageViewModel : INotifyPropertyChanged
     {
-        public Datastructures.ArrayList Stocks { get; set; } = new Datastructures.ArrayList();
+        public Datastructures.ArrayList<Stock> Stocks { get; set; } = new Datastructures.ArrayList<Stock>();
         public Datastructures.DoublyLinkedList<Stock> StocksLinkedList { get; set; } = new Datastructures.DoublyLinkedList<Stock>();
-
+        public ObservableCollection<Stock> StockCollection { get; set; } = new ObservableCollection<Stock>();
+        public string LastAction { get; set; } = "Geen";
+        public string LastActionString => LastAction + " " + stopwatch.ElapsedTicks + " ticks";
+        public Stopwatch stopwatch = new Stopwatch();
         public event PropertyChangedEventHandler? PropertyChanged;
 
         [RelayCommand]
@@ -27,14 +33,17 @@ namespace AlgorithmProgramming.ViewModels
                         Console.WriteLine(jsonString);
                         StockList stockList = JsonSerializer.Deserialize<StockList>(jsonString);
                         Stocks.Clear();
+                        StocksLinkedList.Clear();
+                        StockCollection.Clear();
                         foreach (var stock in stockList.Stocks)
                         {
                             Stocks.Add(stock);
                             StocksLinkedList.Add(stock);
-                        }
-                        OnPropertyChanged(nameof(Stocks));
-                        OnPropertyChanged(nameof(StocksLinkedList));
+                        }                    
                     }
+                    OnPropertyChanged(nameof(Stocks));
+                    OnPropertyChanged(nameof(StocksLinkedList));
+                    SetTableToArrayList();
                 }
 
                 return result;
@@ -49,6 +58,37 @@ namespace AlgorithmProgramming.ViewModels
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [RelayCommand]
+        public void QuickSort()
+        {
+            LastAction = "ArrayList quicksort";
+            stopwatch.Start();
+            Quicksort.Sort(Stocks);
+            stopwatch.Stop();
+            SetTableToArrayList();
+            OnPropertyChanged(nameof(LastActionString));
+        }
+
+        [RelayCommand]
+        public void SetTableToArrayList()
+        {
+            StockCollection.Clear();
+            foreach(Stock stock in Stocks)
+            {
+                StockCollection.Add(stock);
+            }
+        }
+
+        [RelayCommand]
+        public void SetTableToDoublyLinkedList()
+        {
+            StockCollection.Clear();
+            foreach (Stock stock in StocksLinkedList)
+            {
+                StockCollection.Add(stock);
+            }
         }
     }
 }
