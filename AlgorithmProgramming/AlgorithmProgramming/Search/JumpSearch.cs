be600e32a6234bd5e0c2408cc2004ea3d926f2e0
KevinSpa2 <kevin.spalink@student.nhlstenden.com>
2025-03-20 10:48:12 +0100

    Update jumpsearch to work with arraylist

--- AlgorithmProgramming/AlgorithmProgramming/Search/JumpSearch.cs
+++ AlgorithmProgramming/AlgorithmProgramming/Search/JumpSearch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,32 +10,46 @@ namespace AlgorithmProgramming.Search
 {
     public class JumpSearch
     {
-        public static int SearchJump<T>(T[] arr, T key) where T : IComparable<T> 
-        { 
-            int ArraySize = arr.Length;
+        public static int SearchJump<T>(ArrayList list, T key) where T : IComparable<T>
+        {
+            int listSize = list.Count;
             int prev = 0;
-            int step = (int) Math.Sqrt(ArraySize);
+            int step = (int)Math.Sqrt(listSize);
 
-            while (prev < ArraySize && arr[Math.Min(step, ArraySize) - 1].CompareTo(key) < 0)
+            // Go through the list in jumps
+            while (prev < listSize)
             {
-                prev = step;
-                step += (int)Math.Sqrt(ArraySize);
-                if (prev >= ArraySize) 
+                // Check if the first element is not null
+                object? currentElement = list[Math.Min(step, listSize) - 1];
+                if (currentElement != null && ((T)currentElement).CompareTo(key) < 0)
                 {
-                    return -1; //Key is not in array
+                    prev = step;
+                    step += (int)Math.Sqrt(listSize);
+
+                    // If we go outside of the array size the key is not in the list
+                    if (prev >= listSize)
+                    {
+                        return -1; //Key is not in array
+                    }
+                }
+                else
+                {
+                    break; // Stop if the element was found
                 }
             }
-
-            while (prev < Math.Min(step, ArraySize) && arr[prev].CompareTo(key) < 1) 
+            
+            // Lineair search from the last step
+            while (prev < Math.Min(step, listSize))
             {
-                if (arr[prev].Equals(key))
+                object? currentElement = list[prev];
+                if (currentElement != null && ((T)currentElement).CompareTo(key) == 0)
                 {
                     return prev;
                 }
                 prev++;
             }
 
-            return 1; // Key not found
+            return -1; // Key not found
         }
     }
 }
