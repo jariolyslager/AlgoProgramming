39245293219dcf179bf2b24774d5d50f1ae51ef2
KevinSpa <kevin.spalink@student.nhlstenden.com>
2025-03-29 23:06:18 +0100

    Create BubbleSort.cs

---
+++ AlgorithmProgramming/AlgorithmProgramming/Sorting/BubbleSort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmProgramming.Datastructures;
+using AlgorithmProgramming.Models;
+
+namespace AlgorithmProgramming.Sorting
+{
+    internal class BubbleSort
+    {
+        public static HashMap<string, Stock> SortHashMapByPrice(HashMap<string, Stock> inputMap)
+        {
+            var sortedMap = new HashMap<string, Stock>();
+            return sortedMap;
+        }
+    }
+}
