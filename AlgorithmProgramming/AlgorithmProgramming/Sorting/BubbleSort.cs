d834c33648ab5841047ae968c9663ebdb8022df1
KevinSpa <kevin.spalink@student.nhlstenden.com>
2025-03-31 19:41:19 +0200

    Start bubble sort algorithm

--- AlgorithmProgramming/AlgorithmProgramming/Sorting/BubbleSort.cs
+++ AlgorithmProgramming/AlgorithmProgramming/Sorting/BubbleSort.cs
@@ -12,7 +12,25 @@ namespace AlgorithmProgramming.Sorting
     {
         public static HashMap<string, Stock> SortHashMapByPrice(HashMap<string, Stock> inputMap)
         {
+            List<Stock> stockList = new List<Stock>();
             var sortedMap = new HashMap<string, Stock>();
+
+            // Put the hashmap values in a list
+            foreach (var pair in inputMap)
+            {
+                stockList.Add(pair.Value);
+            }
+
+            
+            // (Bubble sort based on price)
+
+
+            // Put sorted values in a hashmap
+            foreach (var stock in stockList)
+            {
+                sortedMap.Add(stock.Ticker, stock);
+            }
+
             return sortedMap;
         }
     }
