4e79ad08a2b8a34df68f50f56e76fe650192c7be
KevinSpa <kevin.spalink@student.nhlstenden.com>
2025-03-31 21:31:14 +0200

    Implemented bubble sort

--- AlgorithmProgramming/AlgorithmProgramming/Sorting/BubbleSort.cs
+++ AlgorithmProgramming/AlgorithmProgramming/Sorting/BubbleSort.cs
@@ -10,19 +10,43 @@ namespace AlgorithmProgramming.Sorting
 {
     internal class BubbleSort
     {
+        /// <summary>
+        /// Sorts a HashMap of stocks in ascending order based on stock price using the Bubble Sort algorithm.
+        /// </summary>
+        /// <param name="inputMap">A HashMap with stock tickers as keys and Stock objects as values.</param>
+        /// <returns>A new HashMap with stocks sorted by price in ascending order.</returns>
         public static HashMap<string, Stock> SortHashMapByPrice(HashMap<string, Stock> inputMap)
         {
-            List<Stock> stockList = new List<Stock>();
+            // Put the hashmap values in a list
+            List<Stock> stockList = new List<Stock>(inputMap.Values);
             var sortedMap = new HashMap<string, Stock>();
 
-            // Put the hashmap values in a list
-            foreach (var pair in inputMap)
-            {
-                stockList.Add(pair.Value);
-            }
+            int listSize = stockList.Count;
+            bool swapped;
 
-            
-            // (Bubble sort based on price)
+            // Bubble sort on the list
+            for (int i = 0; i < listSize - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < listSize - 1 - i; j++)
+                {
+                    // Compare adjacent stock prices
+                    if (stockList[j].Price > stockList[j+1].Price)
+                    {
+                        // Swap if the previous price is higher then the next
+                        var temp = stockList[j];
+                        stockList[j] = stockList[j+1];
+                        stockList[j+1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                // If no swaps happened, the list is already sorted
+                if (!swapped)
+                {
+                    break;
+                }
+            }
 
 
             // Put sorted values in a hashmap
